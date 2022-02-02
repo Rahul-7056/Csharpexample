540fd44e7238d4f44fcaddf5be16d1a5938490d2
rahul <91750@LAPTOP-MACD7JGD>
2022-02-02 17:31:04 +0530

    If Else

--- Csharpexample/Program.cs
+++ Csharpexample/Program.cs
@@ -11,12 +11,14 @@ namespace Csharpexample
             Console.WriteLine("Enter your Last name");
             string b = Console.ReadLine();
             Console.WriteLine("Your Full Name is "+a+" "+b);
+            
             //char
             char first;
             string name = a;
             first = name[0];
             Console.WriteLine("First letter of your name is => "+first+" <= first name is "+ a);
             Console.WriteLine(name[0]);
+            
             //Compare the two numbers
             int num1, num2; bool result;
             Console.WriteLine("Enter First Number");
@@ -35,6 +37,7 @@ namespace Csharpexample
                 result = num1 < num2;
                 Console.WriteLine("Second Number is Greater which is " + result + " and number is " + num2);
             }
+            
             //Constant Keyword
             const double pi = 3.1444; double area, circum;
             Console.WriteLine("Enter the value of Radius of Circle");
@@ -42,7 +45,21 @@ namespace Csharpexample
             area = pi * (radius * radius);
             circum = 2 * pi * radius;
             Console.WriteLine("Area of Circle is "+area); 
-            Console.WriteLine("Circumference of Cirle is "+circum); 
+            Console.WriteLine("Circumference of Cirle is "+circum);
+
+            //If Else Statement
+            Console.WriteLine("Do you want to see the Current Date & Time??");
+            Console.WriteLine("Type Yes or Y For see the Current Date & Time");
+            string ans = Console.ReadLine();
+            if (ans == "Yes" || ans == "Y")
+            {
+                Console.WriteLine("Current Date & Time is " + DateTime.Now.ToString());
+            }
+            else
+            {   
+
+                Console.WriteLine("It's okay :)"); 
+            }
             
             
 
